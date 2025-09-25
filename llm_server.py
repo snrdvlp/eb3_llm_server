bbaecd59706eeb2c6b310d01f556d5cc88fa2b5c
Software Engineer <antibug.mania@gmail.com>
2025-09-25 04:13:34 -0700

    updated health llm

--- llm_server.py
+++ llm_server.py
@@ -60,39 +60,51 @@ async def chat_endpoint(req: LLMRequest):
     except Exception as e:
         return {"error": str(e)}
 
-health_model_id = "aashish12/eb3_quantized"
+health_model_id = "eb3-llm-health/eb3-health"
+health_model_path = "/home/eb3-brayan/eb3-llm-health/eb3-health"
 
 # Load tokenizer
-health_tokenizer = AutoTokenizer.from_pretrained(health_model_id)
+health_tokenizer = AutoTokenizer.from_pretrained(health_model_path, local_files_only=True)
 
 # Load quantized model
 health_model = AutoModelForCausalLM.from_pretrained(
-    health_model_id,
+    health_model_path,
     device_map="auto",        # automatically put on GPU if available
     dtype=torch.float16, # or torch.float16 depending on GPU
     trust_remote_code=True,
-    attn_implementation="flash_attention_2"
-
+    local_files_only=True
 )
 
 @app.post("/chat_health")
-async def generate_text(prompt: str = "hi"):
-    inputs = health_tokenizer(prompt, return_tensors="pt").to(health_model.device)
-    outputs = health_model.generate( **inputs, max_new_tokens=300,  temperature=0.0, top_p=1.0, do_sample=False,  use_cache=True)
-    response_text = health_tokenizer.decode(outputs[0], skip_special_tokens=True)
+async def generate_text(req: LLMRequest):
+    try:
+        messages = [
+            {"role": "system", "content": req.system_prompt},
+            {"role": "user", "content": req.user_prompt}
+        ]
 
-    # Try to extract JSON only
-    start = response_text.find("{")
-    end = response_text.rfind("}")
+        # Build prompt for assistant reply
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True   # 👈 ensures assistant role is ready
+        )
 
-    if start != -1 and end != -1:
-        json_text = response_text[start:end+1]
-        try:
-            data= json.loads(json_text)
-            return data  # returns Python dict
-        except json.JSONDecodeError as e:
-            return {"insurance_details": response_text}
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+        generated_ids = model.generate(
+            **model_inputs,
+            max_new_tokens=req.max_new_tokens,
+            do_sample=False,
+            repetition_penalty=1.0
+        )
 
-    print("\n\n\n\nraw response---->>", response_text)
+        # Decode & clean response
+        response = tokenizer.decode(
+            generated_ids[0][model_inputs["input_ids"].shape[-1]:],
+            skip_special_tokens=True   # 👈 removes <|im_start|>, <|im_end|>, <|endoftext|>
+        ).strip()
 
-    return {"insurance_details": response_text}
+        return {"response": response}
+
+    except Exception as e:
+        return {"error": str(e)}
